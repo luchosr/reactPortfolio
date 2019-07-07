e80d5f065a8a0617c439ea2088b6e24d74c4379f
Luciano <luchosr@gmail.com>
2019-07-07 13:47:04 -0300

    add a Main Header Background Image

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+const Header = () => (
+  <div className="main">
+    <div className="main__header">
+      <div className="main__header__inner">
+        <a href="" className="main__header__inner__avatar">
+          <img src="../src/img/profile.jpg" alt="" />
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
