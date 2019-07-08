0f87b30c2174917400e6246c5834fb877743ebd8
Luciano <luchosr@gmail.com>
2019-07-08 16:05:24 -0300

    add main container

---
+++ src/Main/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./main.css";
+
+const Main = () => (
+  <div className="main">
+    <section className="main__first">
+      <header>
+        <h2>
+          Actually I am based in Buenos Aires, doing my developments using
+          React.js library, Javascript, HTML5 and CSS3.
+        </h2>
+      </header>
+      <p className="main__first__paragraph">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, libero!
+        Quisquam aut totam odio exercitationem inventore magnam quidem mollitia
+        rerum aliquid voluptatem, dolor, numquam eligendi ipsam ullam. Officiis,
+        aperiam placeat.
+      </p>
+      <div className="main__first__paragraph__button">
+        <a href="">Learn More</a>
+      </div>
+    </section>
+  </div>
+);
+
+export default Main;
